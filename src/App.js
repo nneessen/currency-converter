210eb02da2de32ced5d01250fda419502fbdb136
Nick Neessen <nneessen@gmail.com>
2024-02-14 17:28:51 +0300

    completed currency converter react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+function Result({ result }) {
+  return <p>{result}</p>;
+}
+
+export default function App() {
+  const [amount, setAmount] = useState("");
+  const [fromCurrency, setCurrencyFrom] = useState("EUR");
+  const [toCurrency, setCurrencyTo] = useState("USD");
+  const [result, setResult] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleAmountChange(value) {
+    setAmount(value);
+  }
+
+  function handleFromCurrencyChange(value) {
+    setCurrencyFrom(value);
+  }
+
+  function handleToCurrencyChange(value) {
+    setCurrencyTo(value);
+  }
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+      async function fetchData() {
+        setIsLoading(true);
+        try {
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok) throw new Error("Error occured fetching data.");
+          const data = await res.json();
+          setResult(data.rates[toCurrency]);
+          setIsLoading(false);
+        } catch {
+          console.error("Error occured fetching data.");
+        } finally {
+          console.log("Clean up");
+        }
+      }
+      if (fromCurrency == toCurrency) {
+        setResult(amount);
+        setIsLoading(false);
+        return;
+      }
+      fetchData();
+      return () => controller.abort();
+    },
+    [fromCurrency, toCurrency, amount]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        value={amount}
+        type="text"
+        onChange={(e) => handleAmountChange(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => handleFromCurrencyChange(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={toCurrency}
+        onChange={(e) => handleToCurrencyChange(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <Result result={result} />
     </div>
   );
 }
-
-export default App;
